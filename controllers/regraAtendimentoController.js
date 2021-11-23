01f15620aacc5745af8abe731e26732c2fb9c02d
Lucas Oliveira Santana da Cunha <lucas.a.cunha@capgemini.com>
2021-11-23 19:23:30 -0300

    Checkpoint

--- controllers/regraAtendimentoController.js
+++ controllers/regraAtendimentoController.js
@@ -1,22 +1,28 @@
 const fs = require('fs');
 const MESSAGES = require('../utils/messages');
 const { v4: uuidv4 } = require('uuid');
-
+const arquivoRegraAtendimento = './json/regraAtendimento.json'
 module.exports = {
 
-    index(req, res) {
+    async index(req, res) {
         try {
-            return res.status(200).send("Oi")
+            let response;
+            try {
+                response = await JSON.parse(fs.readFileSync(arquivoRegraAtendimento, 'utf8'));
+            } catch (error) {
+                return res.status(404).send({ errors: [{ msg: MESSAGES.JSON_VAZIO_REGRA_ATENDIMENTO }] })
+            }
+            return res.status(200).json(response)
         } catch (error) {
             return res.status(500).send({ errors: [{ msg: MESSAGES.INTERNAL_SERVER_ERROR }] })
         }
     },
 
-    create(req, res){
+    create(req, res) {
         try {
-            const {atendimentoDiario, atendimentoSemanal, diaEspecifico} = req.body
-            if(atendimentoDiario.intervalos) {
-                fs.writeFile('./json/regraAtendimento.json', JSON.stringify({atendimentoDiario: atendimentoDiario}), (err) => {
+            const { atendimentoDiario, atendimentoSemanal, diaEspecifico } = req.body
+            if (atendimentoDiario.intervalos) {
+                fs.writeFile(arquivoRegraAtendimento, JSON.stringify({ atendimentoDiario: atendimentoDiario }), (err) => {
                     if (err) {
                         console.error(err)
                         return res.status(500).send()
@@ -30,9 +36,54 @@ module.exports = {
         }
     },
 
-    horariosDisponiveis(req, res){
+    delete(req, res){
         try {
+            const objRegraAtendimento = await JSON.parse(fs.readFileSync(arquivoRegraAtendimento, 'utf8'));
+            const {id} = req.params
+            let removido = false 
+            objRegraAtendimento.atendimentoDiario.forEach((regraAtendimento, index) => { 
+                if(regraAtendimento.id === id) {
+                    objRegraAtendimento.splice(index, 1)
+                    removido = true
+                    break
+                }
+            })
+            if(removido){
+                
+            }
+            Object.keys(objRegraAtendimento.atendimentoSemanal).forEach((dia) => {
+                dia.forEach((regraAtendimento, index) => { 
+                    if(regraAtendimento.id === id) {
+                        objRegraAtendimento.splice(index, 1)
+                        removido = true
+                    }
+                })
+            })
+            if(removido){
+                
+            }
+            Object.keys(objRegraAtendimento.atendimentoDiaEspecifico).forEach((dia) => {
+                dia.forEach((regraAtendimento, index) => { 
+                    if(regraAtendimento.id === id) {
+                        objRegraAtendimento.splice(index, 1)
+                        removido = true
+                        break
+                    }
+                })
+            })
+            if(removido){
+                
+            }
             
+        } catch (error) {
+            console.error(error)
+            return res.status(500).send({ errors: [{ msg: MESSAGES.INTERNAL_SERVER_ERROR }] })
+        }
+    },
+
+    horariosDisponiveis(req, res) {
+        try {
+
         } catch (error) {
             console.error(error)
             return res.status(500).send({ errors: [{ msg: MESSAGES.INTERNAL_SERVER_ERROR }] })
--- utils/messages.js
+++ utils/messages.js
@@ -1,6 +1,7 @@
 const MESSAGES = {
     'INTERNAL_SERVER_ERROR': 'Internal Server Error',
-    'ERRO_SALVAR_ARQUIVO_JSON': 'Erro ao salvar o arquivo JSON'
+    'ERRO_SALVAR_ARQUIVO_JSON': 'Erro ao salvar o arquivo JSON',
+    'JSON_VAZIO_REGRA_ATENDIMENTO': 'Não há regras de atendimento cadastradas.'
 }
 
 module.exports = MESSAGES
